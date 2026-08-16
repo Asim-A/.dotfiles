01b4ac9149869e79afc5687879559b7aabc9939c
Asim-A <asim.abazi@if.no>
2026-08-16 11:30:27 +0200

    remove lazydev

--- home/dot_config/nvim/lua/plugins/lazydev.lua
+++ home/dot_config/nvim/lua/plugins/lazydev.lua
@@ -11,9 +11,4 @@ return {
     },
   },
   { 'Bilal2453/luvit-meta', lazy = true },
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
 }
